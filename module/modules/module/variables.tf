7135c9068b2717433b62d79a6fcc02652e7b5f8b
GalamManesha <maneeshagalam689@gmail.com>
2025-11-20 11:38:28 +0530

    Update variables.tf

--- module/modules/module/variables.tf
+++ module/modules/module/variables.tf
@@ -3,22 +3,8 @@ variable "instance_type" {
   default = "t3.small"
 }
 
-variable "key_name" {
-  type    = string
-  default = "connect"
-}
-
 variable "ami" {
   type    = string
   default = "ami-0078a63645c7b8a87"
 }
 
-variable "tags" {
-  type    = string
-  default = "variables-hello"
-}
-
-variable "region" {
-  type    = string
-  default = "ap-south-1"
-}
