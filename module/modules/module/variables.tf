85c68c144e8819c11cd7d7c565cb23bf7620aca5
GalamManesha <maneeshagalam689@gmail.com>
2025-11-20 11:41:05 +0530

    Update variables.tf

--- module/modules/module/variables.tf
+++ module/modules/module/variables.tf
@@ -1,10 +1,13 @@
 variable "instance_type" {
-  type    = string
-  default = "t3.small"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
 }
 
 variable "ami" {
-  type    = string
-  default = "ami-0078a63645c7b8a87"
+  description = "AMI id to use"
+  type        = string
+  default     = "ami-0078a63645c7b8a87"
 }
+
 
