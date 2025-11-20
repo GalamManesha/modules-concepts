57f7a22ca4c6c36cfd88b1216f0120cf68ef7516
GalamManesha <maneeshagalam689@gmail.com>
2025-11-20 11:37:43 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,5 +1,6 @@
 module "dev_ec2" {
-  source         = "./modules/module"
-  instance_type  = "t3.small"
-  ami        = "ami-0078a63645c7b8a87"
+  source        = "./modules/module"
+  instance_type = "t3.small"
+  ami           = "ami-0078a63645c7b8a87"
 }
+
